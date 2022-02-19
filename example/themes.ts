d2c119bb0af9b115343df229c6092a254217f33a
Takuya Matsuyama <hi@craftz.dog>
2022-02-19 15:13:18 +0900

    feat(theme): add basic light

--- example/themes.ts
+++ example/themes.ts
@@ -1,7 +1,12 @@
+import { basicLight } from '../packages/basic-light'
 import { solarizedDark } from '../packages/solarized-dark'
 import { solarizedLight } from '../packages/solarized-light'
 
 const themes = [
+  {
+    extension: basicLight,
+    name: 'Basic Light'
+  },
   {
     extension: solarizedLight,
     name: 'Solarized Light'
