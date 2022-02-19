9b920e8b97199a5713bcff31532c5d85f88286b9
Takuya Matsuyama <hi@craftz.dog>
2022-02-19 17:29:30 +0900

    chore(dev): add watch script

---
+++ scripts/watch.js
@@ -0,0 +1,10 @@
+const { watch } = require('@codemirror/buildhelper')
+const { resolve } = require('path')
+const fs = require('fs')
+
+const workspacePath = resolve(__dirname, '..', 'packages')
+const packages = fs
+  .readdirSync(workspacePath)
+  .filter(file => fs.lstatSync(resolve(workspacePath, file)).isDirectory())
+
+watch(packages.map(p => resolve(workspacePath, p, 'src', 'index.ts')))
